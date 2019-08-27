a3ce5619bfbc7ef64787b842e1078f95653663c1
Narnia <software_good@163.com>
2019-08-27 16:20:22 +0800

    add annotations

---
+++ src/main/java/com/example/webthree/concurrency/annotations/ThreadSafe.java
@@ -0,0 +1,17 @@
+package com.example.webthree.concurrency.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author lhx
+ * @date 2019/8/27
+ * 标记【线程安全】的类或写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+    String value() default "";
+}
